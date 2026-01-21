81421e18b4fe1244b59921126f9d1a57574892cf
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-21 17:13:12 +0900

    fix: Correctly parse negative numbers and refine initial character validation in myAtoi.
    Not final version

--- medium/myAtoi.js
+++ medium/myAtoi.js
@@ -2,18 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-s = "words and 987";
+s = "-87";
 
 var myAtoi = function (s) {
   const arr = s.trim().split("");
   const newArr = [];
+  let isNeg = false;
 
   for (let i = 0; i < arr.length; i++) {
     const ascii = arr[i].charCodeAt(0);
 
-    if (arr[0].charCodeAt(0) <= 48 || arr[0].charCodeAt(0) >= 57) {
+    if (arr[0].charCodeAt(0) < 48 || arr[0].charCodeAt(0) > 57) {
       return 0;
     } else {
+      if (arr[0] == "-") {
+        isNeg = true;
+      }
+
       if (ascii >= 48 && ascii <= 57) {
         newArr.push(arr[i]);
       } else {
@@ -22,7 +27,7 @@ var myAtoi = function (s) {
     }
   }
 
-  return parseInt(newArr.join(""));
+  return isNeg ? parseInt(newArr.join("")) * -1 : parseInt(newArr.join(""));
 };
 
 console.log(myAtoi(s));
