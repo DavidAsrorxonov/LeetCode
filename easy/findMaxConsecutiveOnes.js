3ece680c09bd7758f9fc8a094623023bc585375b
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-21 19:28:01 +0900

    fix: correctly calculate maximum consecutive ones by tracking current and maximum counts.

--- easy/findMaxConsecutiveOnes.js
+++ easy/findMaxConsecutiveOnes.js
@@ -6,17 +6,19 @@
 const num1 = [1, 1, 0, 1, 1, 1]; // 6
 
 var findMaxConsecutiveOnes = function (nums) {
-  let count = 0;
+  let current = 0;
+  let max = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 1 && nums[i + 1] === 1) {
-      count = count + 1;
+    if (nums[i] === 1) {
+      current++;
+      max = Math.max(max, current);
     } else {
-      count = 0;
+      current = 0;
     }
   }
 
-  return count;
+  return max;
 };
 
 console.log(findMaxConsecutiveOnes(num1));
