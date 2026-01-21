7980e5c85c3cb9a8378468aee5cbc598d1919990
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-21 17:04:17 +0900

    feat: Implement `myAtoi` function to convert a string to an integer with character validation.

--- medium/myAtoi.js
+++ medium/myAtoi.js
@@ -2,4 +2,27 @@
  * @param {string} s
  * @return {number}
  */
-var myAtoi = function (s) {};
+s = "words and 987";
+
+var myAtoi = function (s) {
+  const arr = s.trim().split("");
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const ascii = arr[i].charCodeAt(0);
+
+    if (arr[0].charCodeAt(0) <= 48 || arr[0].charCodeAt(0) >= 57) {
+      return 0;
+    } else {
+      if (ascii >= 48 && ascii <= 57) {
+        newArr.push(arr[i]);
+      } else {
+        return parseInt(newArr.join(""));
+      }
+    }
+  }
+
+  return parseInt(newArr.join(""));
+};
+
+console.log(myAtoi(s));
